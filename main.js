c6f36f0dda7c6d410cd4271da55d491138d73ba4
Kevin Orellana <alexanderorellana867@gmail.com>
2022-12-22 23:35:48 +0100

    Fusionamos el menu mobile con sus media queries y creamos una funcion en js para que el menu aparezac y desaparezca

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navBarEmail = document.querySelector('.navbar-email');
 const deskTopMenu = document.querySelector('.desktop-menu');
+const menuHamburIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 navBarEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     deskTopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
