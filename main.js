e35ffa5516e5f1afd329ad4f2d9878ebb0b85c9e
Kevin Orellana <alexanderorellana867@gmail.com>
2023-01-03 12:06:23 +0100

    Hemos agregado la sección de los detalles del productos y le hemos dado la funcionabilidad de mostrar y cerrar los productos

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const navBarEmail = document.querySelector('.navbar-email');
 const deskTopMenu = document.querySelector('.desktop-menu');
 const menuHamburIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-
+const productDetails = document.querySelector('.product-detail');
+const navbarShopingCart = document.querySelector('.navbar-shopping-cart');
 
 navBarEmail.addEventListener('click', toggleDesktopMenu);
-menuHamburIcon.addEventListener('click', toggleMobileMenu);
+menuHamburIcon.addEventListener('click', toggleProductDetail);
+navbarShopingCart.addEventListener('click', toggleProductDetail);
 
 function toggleDesktopMenu() {
     deskTopMenu.classList.toggle('inactive');
@@ -13,4 +15,16 @@ function toggleDesktopMenu() {
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleProductDetail() {
+    const isMobileMenuInactive = mobileMenu.classList.contains('inactive');
+    const isProductDetailInactive = productDetails.classList.contains('inactive');
+    if(isMobileMenuInactive){
+        productDetails.classList.add('inactive');
+        mobileMenu.classList.remove('inactive');
+    }else if(isProductDetailInactive){
+        productDetails.classList.remove('inactive');
+        mobileMenu.classList.add('inactive');
+    }
 }
